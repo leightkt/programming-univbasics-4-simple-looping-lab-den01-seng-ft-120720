fd9ca28d79f2c4ed183d8810e4bc7d6cb8c81faa
Kat Leight <leightkt@gmail.com>
2020-05-28 18:31:01 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, num)
+  while num > 0
+   puts message
+   num -= 1
+  end
+end
+
+def output_array(array)
+  array.each do |i|
+    puts i
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  array.each do |i|
+    new_array << i.to_s  
+  end
+  new_array
+end
